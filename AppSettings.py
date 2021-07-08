d500bb0af0ac794aa6d8165c09e6dbceb1f4ff41
Sonia Raposo <soniaraposo.art@gmail.com>
2021-07-08 22:45:38 +0100

    Code Cleanup 2

--- AppSettings.py
+++ AppSettings.py
@@ -1,5 +1,6 @@
 # General settings and data variables for game configuration
 
+<<<<<<< HEAD
 # Modules
 import math
 
@@ -19,12 +20,35 @@ half_y = y // 2
 
 
 """Game Mapping"""  
+=======
+# Biblioteca Math
+import math
+
+"""Screen Configuration"""
+
+x = 800 # Width
+y = 600 # Height
+
+# Frames Per Second
+FPS = 60 
+
+"""Camera position"""
+
+half_x = x // 2
+half_y = y // 2
+
+"""Game Mapping""" 
+>>>>>>> origin/main
 
 # Walls
 walls = 100
 
+<<<<<<< HEAD
 
 """ Ray Casting settings"""
+=======
+""" Ray Casting Settings"""
+>>>>>>> origin/main
 
 # Player Point of View
 pView = math.pi / 3
@@ -46,21 +70,36 @@ proj_3d = 3 * dist * walls
 # Scale Distance
 scale = x // number_rays
 
+<<<<<<< HEAD
 # player settings
 player_pos = (half_x, half_y)
 
 
 """RGB Color CODES"""
+=======
+# Player Position
+player_pos = (half_x, half_y)
+
+
+"""RGB CODES"""
+>>>>>>> origin/main
 
 # Base colors 
 Color1 = (255, 255, 255) # White
 Color2 = (0, 0, 0) # Black
 
+<<<<<<< HEAD
 # Other collors
+=======
+# Other colors
+>>>>>>> origin/main
 Color3 = (0,220,0) # Green
 Color4 = (128,128,128) # Gray
 Color5 = (0,0,204) # Blue
 Color6 = (102, 255, 102) # Unused Green
 Color7 = (23, 54, 5) # Dark Green
+<<<<<<< HEAD
 # Color8 = 
 # Color9 =
+=======
+>>>>>>> origin/main
--- Map.py
+++ Map.py
@@ -22,7 +22,11 @@ GameMap = set()
 for b, row in enumerate(text_map):
     for a, char in enumerate(row):
         if char == 'I':
+<<<<<<< HEAD
             GameMap.add((a * walls, b * walls))
+=======
+            GameMap.add((a * Walls, b * Walls))
+>>>>>>> origin/main
             
 
 # Other cubes (TO BE ADDED)
