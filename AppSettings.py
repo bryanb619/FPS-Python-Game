8e71327b91f31a49b015079020ab35013ebd2eb2
rafaelahenriques <73140603+rafaelahenriques@users.noreply.github.com>
2021-07-14 21:57:33 +0100

    Update AppSettings

--- AppSettings.py
+++ AppSettings.py
@@ -5,25 +5,20 @@ import math
 
 """Screen Configuration"""
 
-res_x = 1200 # Width resolution
-res_y = 800 # Height resolution
+res_x = 1200  # Width resolution
+res_y = 800  # Height resolution
 # Frames Per Second Base Limiter
-FPS = 60 # 60 by default
-
+FPS = 60  # 60 by default
 
 """Camera position"""
 
 half_x = res_x // 2
 half_y = res_y // 2
 
-
-"""Game Mapping"""  
+"""Game Mapping"""
 
 # Walls
 walls = 100
-#
-box = 50
-
 
 """ Ray Casting settings"""
 
@@ -34,15 +29,20 @@ pView = math.pi / 3
 half_pView = pView / 2
 
 # Depth and Angle
+
+# number of rays (MUST BE IN ORDER with screen Width 1200 = 120 rays)
 number_rays = 120
+
+# Maximum depth
 max_depth = 800
 
+# angle
 angle = pView / number_rays
 
 # Object Distance Configuration
 dist = number_rays / (3 * math.tan(half_pView))
 
-# Projection 3D 
+# Projection 3D
 proj_3d = 3 * dist * walls
 
 # Scale Distance
@@ -51,19 +51,17 @@ scale = res_x // number_rays
 # player settings
 player_pos = (half_x, half_y)
 
-
 """RGB Color CODES"""
 
-# Base colors 
-Color1 = (255, 255, 255) # White
-Color2 = (0, 0, 0) # Black
+# Base colors
+Color1 = (255, 255, 255)  # White
+Color2 = (0, 0, 0)  # Black
 
-# Other collors
-Color3 = (0,220,0) # Green
-Color4 = (128,128,128) # Gray
-Color5 = (0,0,204) # Blue
-Color6 = (102, 255, 102) # Unused Green
-Color7 = (23, 54, 5) # Dark Green
-Color8 = (53, 81, 93) # Sky blue 
-
+# Other colors
+Color3 = (0, 220, 0)  # Green
+Color4 = (128, 128, 128)  # Gray
+Color5 = (0, 0, 204)  # Blue
+Color6 = (102, 255, 102)  # Unused Green
+Color7 = (23, 54, 5)  # Dark Green
+Color8 = (53, 81, 93)  # Sky blue
 # Color9 =
