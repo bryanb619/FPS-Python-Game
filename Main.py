68c4cead89edda6d481ca0e0c48bb65cdd50bd09
rafaelahenriques <73140603+rafaelahenriques@users.noreply.github.com>
2021-07-14 15:09:21 +0100

    Code Cleanup & organization

--- Main.py
+++ Main.py
@@ -1,21 +1,17 @@
 """Main FILE"""
 
 # Python Modules
-import pygame, math
+import pygame
 
 # File import
 # Game Main Settings file
 from AppSettings import *
-
 # import player Settings from PlayerScript
 from PlayerScript import Player
-
-# Import Game Map
-from Map import GameMap
-
 # Import Ray Casting from Ray.py
 from Ray import ray_casting
 
+# Import Game Map
 
 """Pygame Window initialization"""
 
@@ -26,10 +22,10 @@ pygame.init()
 pygame.display.set_caption("FPS Application Like")
 
 # Hides mouse cursor ( still there but invisible)
-pygame.mouse.set_cursor((8,8),(0,0),(0,0,0,0,0,0,0,0),(0,0,0,0,0,0,0,0))
+pygame.mouse.set_cursor((8, 8), (0, 0), (0, 0, 0, 0, 0, 0, 0, 0), (0, 0, 0, 0, 0, 0, 0, 0))
 
 """ Screen  """
- 
+
 # Width = X(800), Height = Y(600)
 Screen = pygame.display.set_mode((res_x, res_y))
 
@@ -44,32 +40,30 @@ player = Player()
 while True:
     # Look for event quit
     for event in pygame.event.get():
-         # if "quit" is pressed, app will quit
+        # if "quit" is pressed, app will quit
         if event.type == pygame.QUIT:
             # Kill Game
             quit()
-            
-    """Player, Ground and display config  """       
-     
+
+    """Player, Ground and display config  """
+
     # call movement function (Calls for detected keys) 
     player.movement()
-    
-    #Screen fill color, uses AppSettings Color Scheme "Black"
+
+    # Screen fill color, uses AppSettings Color Scheme "Black"
     Screen.fill(Color2)
-    
+
     # Ground Drawing
     pygame.draw.rect(Screen, Color4, (0, half_y, res_x, half_y))
-    
+
     # Sky drawing
     pygame.draw.rect(Screen, Color8, (0, 0, res_x, half_y))
-    
+
     # Scenario display
     ray_casting(Screen, player.pos, player.angle)
-    
+
     # Displays what was rendered
     pygame.display.flip()
-    
+
     # Game FPS Standard 60 FPS
     clock.tick(FPS)
-   
-            
--- PlayerScript.py
+++ PlayerScript.py
@@ -1,119 +1,108 @@
 """ Player file """
 
 # Modules
-import pygame, math
-
-from pygame.locals import*
-
+import pygame
 # Automatic Module import
-from pygame.constants import KEYDOWN, K_DOWN, K_LSHIFT, K_UP
+from pygame.constants import K_LSHIFT
 
 # import settings
 from AppSettings import *
 
-
 """ Player Settings """
 
 # Angle
-player_angle = 0 # Horizontal
+player_angle = 0  # Horizontal
 
 # Speed
-player_speed = 1 # Horizontal Speed
+player_speed = 1  # Horizontal Speed
 
 PlayerSide = 1
-PlayerBack = 0.2
-
+PlayerBack = 1
 
 # Sprint speed
-Sprint = 1.3 # Horizontal sprinting speed
+Sprint = 1.3  # Horizontal sprinting speed
 
-# player does not jump from begining, so False
-isJumping = False # will be changed to true as soon as "SPACE" is hit
+# player does not jump from beginning, so False
+# isJumping = False # will be changed to true as soon as "SPACE" is hit
 
 # Jump Timer
-CounterJ = 10 
+# CounterJ = 10
 
 
 """ Player Main Class """
+
 
 class Player:
     def __init__(self):
         # X & Y player position
         self.x, self.y = player_pos
-        # Player angle (rotaion)
+        # Player angle (rotation)
         self.angle = player_angle
-        
+
     # Self return
     @property
     def pos(self):
-        return (self.x, self.y)
-    
-    
+        return self.x, self.y
+
     """ Movement function (Detects pressed Keys) """
-    
+
     def movement(self):
         # Movement math
-        sin_a = math.sin(self.angle)
-        cos_a = math.cos(self.angle)
-        
+        sin = math.sin(self.angle)
+        cos = math.cos(self.angle)
+
         """ Keyboard detection """
-        keys = pygame.key.get_pressed()
-        
-        
-        
-        # Special movements   
-            
-        # Forward and Sprint "w + Left Shift" 
-        if keys[pygame.K_w & K_LSHIFT]:
-            self.x += Sprint * player_speed * cos_a
-            self.y += Sprint * player_speed * sin_a
-        
+        buttons = pygame.key.get_pressed()
+
+        # Special movements
+
+        # Forward and Sprint "w + Left Shift"
+        if buttons[pygame.K_w & K_LSHIFT]:
+            self.x += (Sprint + player_speed) * cos
+            self.y += (Sprint + player_speed) * sin
+
         # General Movement
-        
+
         # Forward "k"
-        if keys[pygame.K_w]:
-            self.x += player_speed * cos_a
-            self.y += player_speed * sin_a
+        if buttons[pygame.K_w]:
+            self.x += player_speed * cos
+            self.y += player_speed * sin
         # Back "s"
-        if keys[pygame.K_s]:
-            self.x += -player_speed * cos_a
-            self.y += -player_speed * sin_a
+        if buttons[pygame.K_s]:
+            self.x += -player_speed * cos
+            self.y += -player_speed * sin
         # Left "a"
-        if keys[pygame.K_a]:
-            self.x += player_speed * sin_a
-            self.y += -player_speed * cos_a
-            
+        if buttons[pygame.K_a]:
+            self.x += player_speed * sin
+            self.y += -player_speed * cos
+
         # Right "d"   
-        if keys[pygame.K_d]:
-            self.x += -player_speed * sin_a
-            self.y += player_speed * cos_a 
-            
-        # Left Rotation  "Left Arrow"
-        if keys[pygame.K_LEFT]:
-            self.angle -= 0.02 
-            
-        # Right Rotaion "Right Arrow"
-        if keys[pygame.K_RIGHT]:
+        if buttons[pygame.K_d]:
+            self.x += -player_speed * sin
+            self.y += player_speed * cos
+
+            # Left Rotation  "Left Arrow"
+        if buttons[pygame.K_LEFT]:
+            self.angle -= 0.02
+
+            # Right Rotation "Right Arrow"
+        if buttons[pygame.K_RIGHT]:
             self.angle += 0.02
-               
+
         #   UI Controls  
-        
+
         # Quit Game "ESC"
-        if keys[pygame.K_ESCAPE]: 
+        if buttons[pygame.K_ESCAPE]:
             quit()
-            
-        
+
         """ Mouse detection"""
 
-        #if not(isJumping):  
-            # Jump "Backspace"
-            # if keys[pygame.K_SPACE]:
-                    # Jump is now true
-                    # isJumping = True
-            
-            
-       # Rotation (TO UPDATE TO MOUSE!)  
-    
+        # if not(isJumping):
+        # Jump "Backspace"
+        # if buttons[pygame.K_SPACE]:
+        # Jump is now true
+        # isJumping = True
 
-        # Mouse Controls
+    # Rotation (TO UPDATE TO MOUSE!)
 
+    # Mouse Controls
